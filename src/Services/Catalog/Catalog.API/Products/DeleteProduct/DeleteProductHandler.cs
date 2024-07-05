7f1bdb19615f0fee788ff07f95dcf9cc280040a1
RonLongw <longw_r@hotmail.com>
2024-07-05 17:54:36 +0100

    Add delete product functionality

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Products.GetProductById;
+using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+    internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+            
+            // Save to database
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return result
+            return new DeleteProductResult(true);
+        }
+    }
+}
